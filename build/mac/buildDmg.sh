ccd2bfa4ff640db2ec17671692f94cf95f76361f
magnusknutas <magnus@thefarm.se>
2020-09-19 12:31:18 +0200

    Adding buildfiles

---
+++ build/mac/buildDmg.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+set -x
+################################################################################
+# File:    mac/buildDmg.sh
+# Purpose: Builds a self-contained dmg for a simple Hello World
+#          GUI app using kivy. See also:
+#
+#          * https://kivy.org/doc/stable/installation/installation-osx.html
+#          * https://kivy.org/doc/stable/guide/packaging-osx.html
+#          * https://blog.fossasia.org/deploying-a-kivy-application-with-pyinstaller-for-mac-osx-to-github/
+#
+# Authors: Michael Altfield <michael@buskill.in>
+# Created: 2020-06-22
+# Updated: 2020-09-17
+# Version: 0.2
+################################################################################
+
+
+############
+# SETTINGS #
+############
+
+PYTHON_PATH="`find /usr/local/Cellar/python@3.7 -type f -name python3.7 | head -n1`"
+PIP_PATH="`find /usr/local/Cellar/python@3.7 -type f -name pip3.7 | head -n1`"
+APP_NAME='helloWorld'
+
+PYTHON_VERSION="`${PYTHON_PATH} --version | cut -d' ' -f2`"
+PYTHON_EXEC_VERSION="`echo ${PYTHON_VERSION} | cut -d. -f1-2`"
+
+########
+# INFO #
+########
+
+# print some info for debugging failed builds
+uname -a
+sw_vers
+which python2
+python2 --version
+which python3
+python3 --version
+${PYTHON_PATH} --version
+echo $PATH
+pwd
+ls -lah
+
+###################
+# INSTALL DEPENDS #
+###################
+
+# first update brew
+#  * https://blog.fossasia.org/deploying-a-kivy-application-with-pyinstaller-for-mac-osx-to-github/
+brew update
+
+# install os-level depends
+brew install wget python3
+brew reinstall sdl2 sdl2_image sdl2_ttf sdl2_mixer
+
+# setup a virtualenv to isolate our app's python depends
+sudo ${PYTHON_PATH} -m ensurepip
+${PIP_PATH} install --upgrade --force-reinstall --user pip setuptools
+#${PYTHON_PATH} -m pip install --upgrade --user virtualenv
+#${PYTHON_PATH} -m virtualenv /tmp/kivy_venv
+
+# install kivy and all other python dependencies with pip into our virtual env
+#source /tmp/kivy_venv/bin/activate
+${PIP_PATH} install --upgrade --force-reinstall --user Cython==0.29.10 || exit 1
+${PIP_PATH} install --upgrade --force-reinstall --user -r requirements.txt || exit 1
+${PIP_PATH} install --upgrade --force-reinstall --user PyInstaller || exit 1
+
+#####################
+# PYINSTALLER BUILD #
+#####################
+
+mkdir pyinstaller
+pushd pyinstaller
+
+cat >> ${APP_NAME}.spec <<EOF
+# -*- mode: python ; coding: utf-8 -*-
+
+block_cipher = None
+
+
+a = Analysis(['../src/main.py'],
+             pathex=['./'],
+             binaries=[],
+             datas=[],
+             hiddenimports=['pkg_resources.py2_warn'],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=['_tkinter', 'Tkinter', 'enchant', 'twisted'],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          [],
+          exclude_binaries=True,
+          name='${APP_NAME}',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          console=False )
+coll = COLLECT(exe, Tree('../src/'),
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               upx_exclude=[],
+               name='${APP_NAME}')
+app = BUNDLE(coll,
+             name='${APP_NAME}.app',
+             icon=None,
+             bundle_identifier=None)
+EOF
+
+${PYTHON_PATH} -m PyInstaller -y --clean --windowed "${APP_NAME}.spec"
+
+pushd dist
+hdiutil create ./${APP_NAME}.dmg -srcfolder ${APP_NAME}.app -ov
+popd
+
+#####################
+# PREPARE ARTIFACTS #
+#####################
+
+# create the dist dir for our result to be uploaded as an artifact
+mkdir -p ../dist
+cp "dist/${APP_NAME}.dmg" ../dist/
+
+#######################
+# OUTPUT VERSION INFO #
+#######################
+
+uname -a
+sw_vers
+which python2
+python2 --version
+which python3
+python3 --version
+${PYTHON_PATH} --version
+echo $PATH
+pwd
+ls -lah
+ls -lah dist
+
+##################
+# CLEANUP & EXIT #
+##################
+
+# exit cleanly
+exit 0
